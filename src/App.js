573126f24b870602c05396213521b22677e81b43
Abdul-Wadud Sumaila <abdul.sumaila@amalitech.org>
2022-03-05 23:08:16 +0000

    iniatial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, { useContext } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+const App = () => {
+  const data = useContext(DataContext);
+  
+  return (
+    <div className="app">
+      <Switch>
+        <Route path="/product-detail/:productId">
+          <ProductDetail product={dataCtx.name} />
+        </Route>
+        <Route path="/headphones">
+          <Headphones />
+        </Route>
+        <Route path="/speakers">
+          <Speakers />
+        </Route>
+        <Route path="/earphones">
+          <Earphones />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
